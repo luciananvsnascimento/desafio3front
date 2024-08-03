39a58a2afe77d12c85286916716895198bdee68f
Luciana Neves <luciana_nascimento@hotmail.com.br>
2024-08-03 00:58:54 -0300

    corrigindo o componente hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,18 @@
 import '../styles/hero.css';
-import HeroImg from '../assets/hero-img.jpg';
+import HeroImg from '../assets/hero-img.png';
 
 const Hero = () => {
   return (
     <section className="herohome">
       <img src={HeroImg} alt="Hero" />
+      <div className="homebox">
+        <div className="hero-info">
+          <span className="s-hero">New Arrival</span>
+          <h1>Discover Our <br />New Collection</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut<br />elit tellus, luctus nec ullamcorper mattis.</p>
+          <button className="buy">BUY NOW</button>
+        </div>
+      </div>
     </section>
   );
 }
